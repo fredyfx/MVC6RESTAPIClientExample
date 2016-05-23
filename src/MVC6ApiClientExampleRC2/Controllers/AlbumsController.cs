13e6b0ceb5581922127c2f68925aeac8567c0256
unknown <rschrader@LCLTCS16416.clt.lash.loc>
2016-05-23 14:07:01 -0400

    Initial commit (migrated to Core RC2)

---
+++ src/MVC6ApiClientExampleRC2/Controllers/AlbumsController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using MVC6ApiClientExample.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC6ApiClientExample.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        string _baseUrl = "http://localhost:8000/api/albums/";
+
+        public AlbumsController()
+        {
+
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var requestUrl = _baseUrl;
+            var httpClient = new HttpClient();
+
+            List<Album> albums;
+
+            using (var response = await httpClient.GetAsync(requestUrl))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(String.Format("Server error (HTTP {0}: {1}).", response.StatusCode, response.Content));
+                }
+
+                var json = await response.Content.ReadAsStringAsync();
+                albums = JsonConvert.DeserializeObject(json, typeof(List<Album>)) as List<Album>;
+            }
+
+            return View(albums);
+        }
+
+        public async Task<IActionResult> Detail(string id)
+        {
+            var requestUrl = _baseUrl + id;
+            var httpClient = new HttpClient();
+
+            Album album;
+
+            using (var response = await httpClient.GetAsync(requestUrl))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(String.Format("Server error (HTTP {0}: {1}).", response.StatusCode, response.Content));
+                }
+
+                var json = await response.Content.ReadAsStringAsync();
+                album = JsonConvert.DeserializeObject(json, typeof(Album)) as Album;
+            }
+
+            return View(album);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var requestUrl = _baseUrl + id;
+            var httpClient = new HttpClient();
+
+            Album album;
+
+            using (var response = await httpClient.GetAsync(requestUrl))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(String.Format("Server error (HTTP {0}: {1}).", response.StatusCode, response.Content));
+                }
+
+                var json = await response.Content.ReadAsStringAsync();
+                album = JsonConvert.DeserializeObject(json, typeof(Album)) as Album;
+            }
+
+            return View(album);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Album album)
+        {
+            var requestUrl = _baseUrl + album.Id;
+            var httpClient = new HttpClient();
+
+            using (var response = await httpClient.PutAsync(requestUrl, new StringContent(JsonConvert.SerializeObject(album), Encoding.UTF8, "application/json")))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(String.Format("Server error (HTTP {0}: {1}).", response.StatusCode, response.Content));
+                }
+            }
+
+            //Go back to the list
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var requestUrl = _baseUrl + id;
+            var httpClient = new HttpClient();
+            
+            using (var response = await httpClient.DeleteAsync(requestUrl))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(String.Format("Server error (HTTP {0}: {1}).", response.StatusCode, response.Content));
+                }
+            }
+
+            //Refresh the list
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult Create()
+        {
+            var newAlbum = new Album();
+
+            return View(newAlbum);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Album album)
+        {
+            var requestUrl = _baseUrl;
+            var httpClient = new HttpClient();
+
+            using (var response = await httpClient.PostAsync(requestUrl, new StringContent(JsonConvert.SerializeObject(album), Encoding.UTF8, "application/json")))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(String.Format("Server error (HTTP {0}: {1}).", response.StatusCode, response.Content));
+                }
+            }
+
+            //Go back to the list
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ src/MVC6ApiClientExampleRC2/Models/Album.cs
@@ -0,0 +1,17 @@
+﻿
+using Newtonsoft.Json;
+
+namespace MVC6ApiClientExample.Models
+{
+    public class Album
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("artist")]
+        public string Artist { get; set; }
+    }
+}
