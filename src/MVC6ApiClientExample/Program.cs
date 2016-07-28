bdc3c7fc28caa75e6af71deabff12a390ab9786b
unknown <rschrader@LCLTCS16416.clt.lash.loc>
2016-07-28 11:23:34 -0400

    Updated to .NET Core 1.0

--- src/MVC6ApiClientExample/Program.cs
+++ src/MVC6ApiClientExample/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace MVC6ApiClientExampleRC2
+namespace MVC6ApiClientExample
 {
     public class Program
     {
--- src/MVC6ApiClientExample/Startup.cs
+++ src/MVC6ApiClientExample/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace MVC6ApiClientExampleRC2
+namespace MVC6ApiClientExample
 {
     public class Startup
     {
